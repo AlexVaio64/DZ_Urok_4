e88714bb04ad5b2b2ca4742b11b4d2549c0ed319
Michel Nekrasov <moscou@inbox.ru>
2022-11-09 23:14:23 +0300

    add Zadacha 27 finished

--- Zadacha_27/Program.cs
+++ Zadacha_27/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число X: ");
+int numberX = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberX)
+  {    
+    int counter = Convert.ToString(numberX).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberX - numberX % 10;
+      result = result + (numberX - advance);
+      numberX = numberX / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberX);
+Console.WriteLine("Сумма цифр в числе = " + sumNumber);
